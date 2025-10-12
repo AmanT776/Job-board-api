ea7d69bfc70f843bc5cc6578e802af2104fb346b
AmanT776 <amantes776@gmail.com>
2025-10-12 20:30:56 +0300

    feat:-created token based authentication

--- users/serializers.py
+++ users/serializers.py
@@ -1,20 +1,30 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import CustomUser
+from rest_framework.response import Response
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id','first_name','last_name','phone','role','email','password']
+        extra_kwargs = {'password': {'write_only': True}}
     def create(self, validated_data):
         password = validated_data.pop('password',None)
         user = self.Meta.model(**validated_data)
         if password:
             user.set_password(password)
+        else:
+            raise ValueError('password is required') 
         user.save()
         return user
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     username_field = 'email'
 
+    @classmethod
+    def get_token(cls,user):
+        token = super().get_token(user)
+        if token == 'password':
+            del token['password']
+        return token
 
--- users/views.py
+++ users/views.py
@@ -5,12 +5,19 @@ from .serializers import (
     UserSerializer,CustomTokenObtainPairSerializer
 )
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
