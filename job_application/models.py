3eef2bc3030bb1ebb318d8018bb06f90dd8365ca
AmanT776 <amantes776@gmail.com>
2025-10-13 20:51:01 +0300

    feat:-created job model

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from users.models import CustomUser
 # Create your models here.
+from users.models import CustomUser
+# Create your models here.
+class Job(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    employer = models.ForeignKey(CustomUser,related_name="jobs",on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=30)
+    salary = models.FloatField()
+
